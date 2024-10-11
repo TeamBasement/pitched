6d5ca20aad0d1ad2f792d7d906b1b683f0609401
Tristan Rasmussen <tristanrasmussen@tristanrasmussen.com>
2024-10-11 14:18:55 -0400

    refactor: Improve AudioManager class structure and add pitch detection feature

--- src/audio/AudioManager.ts
+++ src/audio/AudioManager.ts
@@ -1,47 +1,53 @@
-export class AudioManager {
-  private audioContext: AudioContext | null = null;
-  private analyser: AnalyserNode | null = null;
-  private dataArray: Float32Array | null = null;
-  private bufferLength: number = 0;
+import { PitchDetector } from "pitchy";
 
-  public initialize() {
+export class AudioManager {
+  private readonly audioContext: AudioContext;
+  private readonly analyser: AnalyserNode;
+  private readonly dataArray: Float32Array;
+  private readonly pitchDetector: PitchDetector<Float32Array>;
+  private readonly bufferLength: number = 0;
+
+  public constructor() {
     this.audioContext = new window.AudioContext();
     this.analyser = this.audioContext.createAnalyser();
     this.analyser.fftSize = 2048;
     this.bufferLength = this.analyser.frequencyBinCount;
     this.dataArray = new Float32Array(this.bufferLength);
+    this.pitchDetector = PitchDetector.forFloat32Array(this.analyser.fftSize);
   }
+
+  public initialize() {}
 
   public shutdown() {
     if (this.audioContext) {
       this.audioContext.close();
-      this.audioContext = null;
     }
   }
 
   public async beginListening() {
-    if (!this.audioContext) {
-      throw new Error("AudioContext is not initialized.");
-    }
-
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
     const source = this.audioContext.createMediaStreamSource(stream);
     source.connect(this.analyser!);
   }
 
-  public getTimeData(): Float32Array | null {
-    if (this.analyser && this.dataArray) {
-      this.analyser.getFloatTimeDomainData(this.dataArray);
-      return this.dataArray;
-    }
-    return null;
+  public getTimeData(): Float32Array {
+    this.analyser.getFloatTimeDomainData(this.dataArray);
+    return this.dataArray;
   }
 
-  public getFrequencyData(): Float32Array | null {
-    if (this.analyser && this.dataArray) {
-      this.analyser.getFloatFrequencyData(this.dataArray);
-      return this.dataArray;
-    }
-    return null;
+  public getFrequencyData(): Float32Array {
+    this.analyser.getFloatFrequencyData(this.dataArray);
+    return this.dataArray;
   }
+
+  public getPitch(): { pitch: number; confidence: number } {
+    const data = this.getTimeData();
+    const [pitch, confidence] = this.pitchDetector.findPitch(
+      data,
+      this.audioContext.sampleRate
+    );
+    return { pitch, confidence };
+  }
+
+  public debug_connectSineWave() {}
 }
