6c5ac13b28fa7176aac54c63f0dd313237c6ab13
Tristan Rasmussen <tristanrasmussen@tristanrasmussen.com>
2024-10-11 14:02:28 -0400

    refactor: initialize audio manager using appropriate react pieces

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import './App.css';
-import { AudioManager } from './audio/AudioManager';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { AudioManager } from "./audio/AudioManager";
 
 function App() {
-  const audioManager = new AudioManager();
+  const [audioManager, setAudioManager] = useState<AudioManager | null>(null);
 
-  const handleInitializeAudio = () => {
-    audioManager.initialize();
-    console.log("AudioManager initialized");
-  };
+  useEffect(() => {
+    const audioManager = new AudioManager();
+    setAudioManager(audioManager);
+    return () => audioManager.shutdown();
+  }, []);
+
+  return (
     <div>
       <h1>Pitched</h1>
-      <p>Welcome to Pitched, an app that plays with pitch detection and replication.</p>
-      <button onClick={handleInitializeAudio}>Initialize Audio</button>
+      <p>
+        Welcome to Pitched, an app that plays with pitch detection and
+        replication.
+      </p>
+      <button onClick={() => audioManager?.initialize()}>Begin</button>
     </div>
   );
 }
